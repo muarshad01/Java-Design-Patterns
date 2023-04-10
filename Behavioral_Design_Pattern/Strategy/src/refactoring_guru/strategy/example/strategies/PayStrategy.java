97fa8f18afd5d9ffe96c8ef15a053f875ed5d9d2
Muhammad Arshad <marshad@marshad-ltmnwup.internal.salesforce.com>
2023-04-10 15:52:02 -0400

    bdp

---
+++ Behavioral_Design_Pattern/Strategy/src/refactoring_guru/strategy/example/strategies/PayStrategy.java
@@ -0,0 +1,8 @@
+package refactoring_guru.strategy.example.strategies;
+/**
+ * Common interface for all strategies.
+ */
+public interface PayStrategy {
+    boolean pay(int paymentAmount);
+    void collectPaymentDetails();
+}
